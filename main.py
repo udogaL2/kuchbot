bed7f8dcfb22fe8b2e313b4bb1ba9d9c59ecab60
udogaL2 <57914563+udogaL2@users.noreply.github.com>
2021-01-21 21:49:17 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('1522582454:AAGK3_IIqjkFzQR1VsezBMku3a181vU3mq0')
+
+
+@bot.message_handler(commands=['start'])
+def welcome(msg):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Где куч?")
+    item2 = types.KeyboardButton("Через сколько придет куч?")
+
+    markup.add(item1, item2)
+    bot.send_message(msg.chat.id,
+                     'Добро пожаловать, {0.first_name}!\nМеня зовут <b>"{1.first_name} - бот"</b>. Я расскажу тебе всё о местоположении куча на данный момент.😊'.format(
+                         msg.from_user, bot.get_me()),
+                     parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def ans(msg):
+    if msg.chat.type == 'private':
+        if msg.text == 'Где куч?':
+            bot.send_message(msg.chat.id, 'Он скоро придёт.')
+        elif msg.text == 'Через сколько придет куч?':
+            bot.send_message(msg.chat.id, 'Он придёт через 3 секунды.')
+        else:
+            bot.send_message(msg.chat.id, 'Зачем мне писать что-то, кроме сообщений о местоположении куча?')
+
+
+bot.polling(none_stop=True)
